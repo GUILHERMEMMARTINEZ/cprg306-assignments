bc9765a6f7acd8f452101a58cb91850f55752645
GUILHERMEMMARTINEZ <99258374+GUILHERMEMMARTINEZ@users.noreply.github.com>
2024-05-18 23:53:04 -0600

    Completed week 2 assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Guilherme de Andrade Martinez</p>
+      <Link href="https://github.com/GUILHERMEMMARTINEZ/cprg306-assignments">
+        GitHub Repository
+      </Link>
+    </div>
+  );
+}
